0196b7683d251e9aebc0dbd15157831b5b90d2ab
zhaoblake <zhao_decheng@qq.com>
2024-01-29 15:21:10 +0800

    feat: support use from command line

--- cmd/com2see.go
+++ cmd/com2see.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"com2see/internal"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
 
+func main() {
+	var path string
+	var year int
+
+	flag.StringVar(&path, "path", "", "Your git repository path, defaults to current working directory")
+	flag.IntVar(&year, "year", time.Now().Year()-1, "Year for the commit report, defaults to last year")
+
+	flag.Parse()
+	err := internal.GenerateHTMLReport(path, year)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
